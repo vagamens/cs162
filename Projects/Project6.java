14a9f3ae8f1bb511b7d91b8e545640fa43b34e5c
Kevin Clay <clay.kvn.c@gmail.com>
2016-09-29 18:53:56 -0700

    File Updates
    Added prompt to Project 6
    
    Changes to be committed:
    	modified:   Projects/Project6.java

--- Projects/Project6.java
+++ Projects/Project6.java
@@ -1,2 +1,10 @@
 /* Project 6
    pg481: 9.7 (Car)
+
+   Write a class called MonetaryCoin that is derived from the Coin class
+   presented in Chapter 5. Store an integer in the MonetaryCoin that represents
+   its value and add a method that returns its value. Create a mian driver
+   class to instantiate and compute the sum of several MonetaryCoin objects.
+   Demonstrate that a monetary coin inherits its parent's ability to be flipped.
+*/
+
