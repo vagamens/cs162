c1162ff5b69cfb47bddc442bcfc4f7d1461e8486
Kevin Clay <clay.kvn.c@gmail.com>
2016-09-29 17:52:13 -0700

    File Rename
    Renamed Exercise2.java to Exercise2.md
    
    Changes to be committed:
    	renamed:    PracticeExercies/Exercise2.java -> PracticeExercies/Exercise2.md
    	modified:   Projects/Project2.java

--- Projects/Project2.java
+++ Projects/Project2.java
@@ -1,13 +1,16 @@
 /* Project2.java
    pg436 project 8.8+ (File I/O)
 
-   write a program that reads a sequence of up to 25 pairs of names and postal
+   Write a program that reads a sequence of up to 25 pairs of names and postal
    (ZIP) codes for individuals. Store the data in an object designed to store
-   a first name (string), lastname (string), and postal code (integer). assume
+   a first name (string), lastname (string), and postal code (integer). Assume
    each line of input will contain two strings followed by an integer value,
-   each separated by a tab character. then, after the input has been read in,
+   each separated by a tab character. Then, after the input has been read in,
    print the list in an appropriate format to the screen.
 */
+
+import java.util.File;
+import java.util.Scanner;
 
 class Project2 {
 	public static void main(String[] args) {
