bc4fd02b29904db330cf649cd600f992fa340765
Kevin Clay <clay.kvn.c@gmail.com>
2016-09-29 18:56:34 -0700

    File Updates
    Added prompt to Project 7.
    
    Changes to be committed:
    	modified:   Projects/Project7.java

--- Projects/Project7.java
+++ Projects/Project7.java
@@ -1,2 +1,10 @@
 /* Project 7
    pg534: 10.6 (Graphic Sort)
+
+   Write a program that graphically displays the processing of a selection
+   sort. Use bars of various heights to represent the values being sorted.
+   Display the set of bars after each swap. Put a delay in the processing of
+   the sort to give the human observer a chance to see how the order of the
+   values changes.
+*/
+
