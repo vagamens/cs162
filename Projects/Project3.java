f38b69c02f3bde62a1f41c7b8626fb2a0f0adcc6
Kevin Clay <clay.kvn.c@gmail.com>
2016-09-29 18:50:14 -0700

    File Updates
    Added prompt to Project 3
    
    Changes to be committed:
    	modified:   Projects/Project3.java

--- Projects/Project3.java
+++ Projects/Project3.java
@@ -1,2 +1,10 @@
 /* Project 3
    pg438: 8.16 (Circles)
+
+   Write a program that draws 20 circles, with the radious and location of each
+   circle determined at random. If a circle does not overlap any other circle,
+   draw that circle in black. If a circle overlaps one or more other circle,
+   then determine the color of each circle. Two circles overlap if the distance
+   between their center points is less than the sum of eithr radii.
+*/
+
