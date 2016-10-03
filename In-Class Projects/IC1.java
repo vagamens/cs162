49fc103aa00dd534d903643a152e114c5c56160f
Kevin Clay <clay.kvn.c@gmail.com>
2016-10-03 10:42:34 -0700

    File Updates
    added files and folders for take-home assignments and in-class projects
    
    Changes to be committed:
    	new file:   In-Class Projects/IC1.java
    	new file:   Take-Home Assignments/TH1.java

---
+++ In-Class Projects/IC1.java
@@ -0,0 +1,15 @@
+/* In-Class Project 1
+   1. design and create aclass to represent a somewhat complex object (car,
+   house, computer, being, container structure) including:
+   		1. a few attributes (variables).
+   		2. a few behaviors (methods).
+   		3. a simple menu to:
+   			1. get basic input from a user,
+   			2. display basic info about your object
+   			3. a few tests that would show evidence that your program is
+   				working correctly.
+*/
+
+public class IC1 {
+	
+}
