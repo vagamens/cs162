c0ce6b19c09e2cbb05f26f1559a164111471da70
Kevin Clay <clay.kvn.c@gmail.com>
2016-10-03 10:53:43 -0700

    File Update
    Added Take-Home Assignments
    
    Added prompt for TH1
    
    Changes to be committed:
    	modified:   Take-Home Assignments/TH1.java
    	new file:   Take-Home Assignments/TH2.java
    	new file:   Take-Home Assignments/TH3.java
    	new file:   Take-Home Assignments/TH4.java
    	new file:   Take-Home Assignments/TH5.java
    	new file:   Take-Home Assignments/TH6.java
    	new file:   Take-Home Assignments/TH7.java
    	new file:   Take-Home Assignments/TH8.java
    	new file:   Take-Home Assignments/TH9.java

---
+++ Take-Home Assignments/TH1.java
@@ -0,0 +1,32 @@
+/* Take-Home Assignment 1
+   
+   Make a program that has or can do the following:
+
+   Note Well!: you do not have to actually read or write a file for this
+   assignment, just setup the initial user input, and confirmation of input as
+   output you can verify visually.
+
+   Note: you should have a few methods to accomplish these, but you do not
+   necessarily need a method for each.
+
+   1. a text-based menu for the user to interact with this program (numbers for
+   menu input is fine)
+   2. ask for the name of a file, ask for a filename to writeto, and print the
+   value back out to confirm the input with your eyes
+   3. ask for text to be written to a file, ask for a filename to write to, and
+   print the value back out to confirm the input with your eyes
+   4. a menu item that when selected will ask the user for a number between 1
+   and 10 (inclusive), verify that that number was input, generate that many
+   random numbers from 1-100, and when done generating numbers print a message
+   indicating how many of each of the number generated were in the following
+   ranges: 1-10, 11-20, 21-30, ... 91-100 (store these in an array, but without
+   storing each of the generated numbers directly)
+   5. quit the program and say goodbye
+   6. loop back to the menu after finishing an item (definitely do not invoke
+   the main method from within the program...) and ask what the user would like
+   to do next.
+*/
+
+public class TH1 {
+	
+}
