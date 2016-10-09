0409ec7c4afa8ec4731e212236e764cd67ef68f8
Kevin Clay <clay.kvn.c@gmail.com>
2016-10-08 17:24:06 -0700

    Final Project
    Added java file and folder for all
    file for the final project.
    
    Added prompt for final project
    
    Changes to be committed:
    	new file:   Final Project/FinalProject.java

---
+++ Final Project/FinalProject.java
@@ -0,0 +1,16 @@
+/* Final Project
+
+   Demonstrate the following:
+    1. an understanding of the use of variables, conditionals,
+       loops, and arrays
+    2. an understanding of code organization (methods, classes,
+       and source files)
+    3. an understanding of various design (hierarchy) and testing
+       (test methods and drivers) techniques
+    4. user IO, fiel IO, and input validation
+    5. java interfaces
+    6. recursion
+    7. nested classes
+    8. GUI components, event listeners, and their various uses
+    9. inheritance and polymorphism
+   10. exceptions
